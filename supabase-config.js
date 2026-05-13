03996be29f5897c1edc96d13ceac5e184d39a1b0
Froggyegglet <josephwuxuzhou@gmail.com>
2026-05-13 15:25:31 +1000

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.EVENTRIG_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://pxvrurjtzxyuzoodsjxw.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4dnJ1cmp0enh5dXpvb2Rzanh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MzA5ODksImV4cCI6MjA5NDIwNjk4OX0.298AUa42Yh65jL5sHklfXbzgHF9SdY5W-DuyVBolFGU"
 };
